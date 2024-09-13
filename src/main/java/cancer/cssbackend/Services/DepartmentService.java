a8a1201a4af237159d4a185f4ff055735802c942
nathan callang <nbcallang@up.edu.ph>
2024-09-13 23:49:04 +0800

    [Feat][Nathan] - added department table

---
+++ src/main/java/cancer/cssbackend/Services/DepartmentService.java
@@ -0,0 +1,11 @@
+package cancer.cssbackend.Services;
+
+import cancer.cssbackend.Repositories.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DepartmentService {
+    private final DepartmentRepository departmentRepository;
+}
