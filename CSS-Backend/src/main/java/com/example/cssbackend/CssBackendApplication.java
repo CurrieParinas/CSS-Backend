a02fd7f601beec9992738a7c7427877c04e8cca1
nathan callang <nbcallang@up.edu.ph>
2024-09-13 12:55:24 +0800

    Initial setup

---
+++ CSS-Backend/src/main/java/com/example/cssbackend/CssBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cssbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CssBackendApplication.class, args);
+    }
+
+}
---
+++ CSS-Backend/src/test/java/com/example/cssbackend/CssBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cssbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
