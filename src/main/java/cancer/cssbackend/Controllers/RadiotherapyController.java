373430cf44071d07eef8bb1e3ed745d2089b1f12
nathan callang <nbcallang@up.edu.ph>
2024-11-02 21:15:10 +0800

    [feat][nathan] - fetch radiotherapy doctors / by facility

--- src/main/java/cancer/cssbackend/Controllers/RadiotherapyController.java
+++ src/main/java/cancer/cssbackend/Controllers/RadiotherapyController.java
@@ -1,12 +1,15 @@
 package cancer.cssbackend.Controllers;
 
 import cancer.cssbackend.Entities.Chemotherapy;
+import cancer.cssbackend.Entities.Doctor;
 import cancer.cssbackend.Entities.Radiotherapy;
 import cancer.cssbackend.Entities.Requests.AddChemotherapyRequest;
 import cancer.cssbackend.Entities.Requests.AddRadiotherapyRequest;
 import cancer.cssbackend.Services.RadiotherapyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -18,5 +21,15 @@ public class RadiotherapyController {
     @PostMapping("/add")
     public Radiotherapy addRadiotherapy(@RequestBody AddRadiotherapyRequest addRadiotherapyRequest) {
         return radiotherapyService.addRadiotherapy(addRadiotherapyRequest);
+    }
+
+    @GetMapping("/findalldoctors")
+    public List<Doctor> fetchAllRadiotherapyDoctors(){
+        return radiotherapyService.fetchAllRadiotherapyDoctors();
+    }
+
+    @GetMapping("/finddoctorsbyfacility")
+    public List<Doctor> findDoctorsByFacility(@RequestParam(value="facilityID") Long facilityID){
+        return radiotherapyService.fetchRadiotherapyDoctorsByFacility(facilityID);
     }
 }
--- src/main/java/cancer/cssbackend/Repositories/RadiotherapyRepository.java
+++ src/main/java/cancer/cssbackend/Repositories/RadiotherapyRepository.java
@@ -2,8 +2,19 @@ package cancer.cssbackend.Repositories;
 
 import cancer.cssbackend.Entities.Radiotherapy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.print.Doc;
+import java.util.List;
 
 @Repository
 public interface RadiotherapyRepository extends JpaRepository<Radiotherapy, Long> {
+    @Query(value = "SELECT DISTINCT d.DOCTOR_ID FROM DOCTOR d JOIN RADIOTHERAPY r ON r.RADRX_DOCTOR = d.DOCTOR_ID", nativeQuery = true)
+
+    List<Long> fetchAllRadiotherapyDoctors();
+
+    @Query(value = "SELECT DISTINCT d.DOCTOR_ID FROM DOCTOR d JOIN RADIOTHERAPY r ON r.RADRX_DOCTOR = d.DOCTOR_ID WHERE r.RADRX_FACILITY= :facilityID ", nativeQuery = true)
+    List<Long> fetchRadiotherapyDoctorsByFacility(@Param("facilityID") Long facilityID);
 }
