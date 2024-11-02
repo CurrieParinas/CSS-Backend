0b69e43e0eb7386902d2488bcb844618fdfc563a
nathan callang <nbcallang@up.edu.ph>
2024-11-02 22:09:37 +0800

    [feat][nathan] fetch all chemotherapy doctors / by facility

--- src/main/java/cancer/cssbackend/Controllers/ChemotherapyController.java
+++ src/main/java/cancer/cssbackend/Controllers/ChemotherapyController.java
@@ -1,12 +1,15 @@
 package cancer.cssbackend.Controllers;
 
 import cancer.cssbackend.Entities.Chemotherapy;
+import cancer.cssbackend.Entities.Doctor;
 import cancer.cssbackend.Entities.Patient;
 import cancer.cssbackend.Entities.Requests.AddChemotherapyRequest;
 import cancer.cssbackend.Entities.Requests.AddPatientRequest;
 import cancer.cssbackend.Services.ChemotherapyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -18,5 +21,15 @@ public class ChemotherapyController {
     @PostMapping("/add")
     public Chemotherapy addChemotherapy(@RequestBody AddChemotherapyRequest addChemotherapyRequest) {
         return chemotherapyService.addChemotherapy(addChemotherapyRequest);
+    }
+
+    @GetMapping("/findalldoctors")
+    public List<Doctor> fetchAllChemotherapyDoctors(){
+        return chemotherapyService.fetchAllDoctors();
+    }
+
+    @GetMapping("/finddoctorsbyfacility")
+    public List<Doctor> fetchDoctorsByFacility(@RequestParam(value="facilityID") Long facilityID){
+        return chemotherapyService.fetchDoctorsByFacility(facilityID);
     }
 }
