99ac5ca3ae4370585d5e06bb8a71201bee640596
nathan callang <nbcallang@up.edu.ph>
2024-11-02 21:46:54 +0800

    [feat][nathan] - immmunotherapy doctors / by facility

--- src/main/java/cancer/cssbackend/Repositories/ImmunotherapyRepository.java
+++ src/main/java/cancer/cssbackend/Repositories/ImmunotherapyRepository.java
@@ -1,9 +1,19 @@
 package cancer.cssbackend.Repositories;
 
+import cancer.cssbackend.Entities.Doctor;
 import cancer.cssbackend.Entities.Immunotherapy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ImmunotherapyRepository extends JpaRepository<Immunotherapy, Long> {
+    @Query(value="SELECT DISTINCT d.DOCTOR_ID FROM DOCTOR d JOIN IMMUNOTHERAPY i ON i.IMMUNORX_DOCTOR = d.DOCTOR_ID", nativeQuery = true)
+    public List<Long> fetchAllDoctors();
+
+    @Query(value = "SELECT DISTINCT d.DOCTOR_ID FROM DOCTOR d JOIN IMMUNOTHERAPY i ON i.IMMUNORX_DOCTOR = d.DOCTOR_ID WHERE i.IMMUNORX_FACILITY= :facilityID", nativeQuery = true)
+    public List<Long> fetchImmunotherapyDoctorsByFacility(@Param("facilityID") Long facilityID);
 }
--- src/main/java/cancer/cssbackend/Services/ImmunotherapyService.java
+++ src/main/java/cancer/cssbackend/Services/ImmunotherapyService.java
@@ -1,18 +1,24 @@
 package cancer.cssbackend.Services;
 
+import cancer.cssbackend.Entities.Doctor;
 import cancer.cssbackend.Entities.Immunotherapy;
 import cancer.cssbackend.Entities.Patient;
 import cancer.cssbackend.Entities.Requests.AddImmunotherapyRequest;
+import cancer.cssbackend.Repositories.DoctorRepository;
 import cancer.cssbackend.Repositories.ImmunotherapyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.print.Doc;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ImmunotherapyService {
     private final ImmunotherapyRepository immunotherapyRepository;
+    private final DoctorRepository doctorRepository;
     private final PatientService patientService;
     private final DoctorService doctorService;
     private final UserService userService;
@@ -22,5 +28,29 @@ public class ImmunotherapyService {
         Immunotherapy immunotherapy = addImmunotherapyRequest.mapToImmunotherapy(patientService, doctorService, userService, hospitalService);
         immunotherapyRepository.save(immunotherapy);
         return immunotherapy;
+    }
+
+    public List<Doctor> fetchAllDoctors(){
+        List<Long> doctorIDs = immunotherapyRepository.fetchAllDoctors();
+        List<Doctor> doctorList = new ArrayList<>();
+
+        for(Long id : doctorIDs){
+            Optional<Doctor> x = doctorRepository.findById(id);
+            x.ifPresent(doctorList::add);
+        }
+
+        return doctorList;
+    }
+
+    public List<Doctor> fetchDoctorsByFacility(Long facilityID){
+        List<Long> doctorIDs = immunotherapyRepository.fetchImmunotherapyDoctorsByFacility(facilityID);
+        List<Doctor> doctorList = new ArrayList<>();
+
+        for(Long id : doctorIDs){
+            Optional<Doctor> x = doctorRepository.findById(id);
+            x.ifPresent(doctorList::add);
+        }
+
+        return doctorList;
     }
 }
