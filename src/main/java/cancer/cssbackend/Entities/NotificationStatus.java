f16210ce18ddd8a2471e2c1505a386778be08c6c
nathan callang <nbcallang@up.edu.ph>
2024-11-03 20:27:41 +0800

    [feat][nathan] notif type spring

---
+++ src/main/java/cancer/cssbackend/Entities/NotificationStatus.java
@@ -0,0 +1,24 @@
+package cancer.cssbackend.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "NOTIFICATIONSTATUS")
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class NotificationStatus {
+    @Id
+    @SequenceGenerator(name = "NOTIFICATIONSTATUS_SEQ", sequenceName = "NOTIFICATIONSTATUS_SEQ", allocationSize = 1)
+    @Column(name = "NOTIFSTATUS_ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "NOTIFICATIONSTATUS_SEQ")
+    private Long notifStatusID;
+
+
+    @Column(nullable = false, length = 200)
+    private String notifStatusName;
+}
