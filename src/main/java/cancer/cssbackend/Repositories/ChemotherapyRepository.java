dd98444a6f879a276db6b13702c135e4097aad7a
nathan callang <nbcallang@up.edu.ph>
2024-11-03 23:58:00 +0800

    [feat][nathan] fetch chemotherapy facilities

--- src/main/java/cancer/cssbackend/Repositories/ChemotherapyRepository.java
+++ src/main/java/cancer/cssbackend/Repositories/ChemotherapyRepository.java
@@ -15,4 +15,8 @@ public interface ChemotherapyRepository extends JpaRepository<Chemotherapy, Long
 
     @Query(value = "SELECT DISTINCT d.DOCTOR_ID FROM DOCTOR d JOIN CHEMOTHERAPY c ON c.CHEMO_DOCTOR = d.DOCTOR_ID WHERE c.CHEMO_FACILITY= :facilityID", nativeQuery = true)
     public List<Long> fetchDoctorsByFacility(@Param("facilityID") Long facilityID);
+
+    @Query(value = "SELECT DISTINCT H.HOSPITAL_ID FROM HOSPITAL H JOIN CHEMOTHERAPY C ON H.HOSPITAL_ID = C.CHEMO_FACILITY", nativeQuery = true)
+    public List<Long> fetchChemotherapyFacilities();
+
 }
