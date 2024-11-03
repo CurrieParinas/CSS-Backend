c63d60c29888ee52f391dee6e8227eacdbd07ce2
Ron Brylle Gomez <136672572+rsgomez1@users.noreply.github.com>
2024-11-03 23:02:48 +0800

    [FEAT][RON] Add Disease Status + Consult

---
+++ src/main/java/cancer/cssbackend/Entities/Requests/AddConsultRequest.java
@@ -0,0 +1,55 @@
+package cancer.cssbackend.Entities.Requests;
+
+import cancer.cssbackend.Entities.Consult;
+import cancer.cssbackend.Entities.DiseaseStatus;
+import cancer.cssbackend.Entities.Patient;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class AddConsultRequest {
+    @JsonProperty("PATIENT_ID")
+    private Long patientID;
+
+    @JsonProperty("CONSULT_SUBJECTIVE")
+    private String consultSubjective;
+
+    @JsonProperty("CONSULT_OBJECTIVE")
+    private String consultObjective;
+
+    @JsonProperty("CONSULT_SURVWORKUP")
+    private String consultSurvworkup;
+
+    @JsonProperty("CONSULT_RXPLAN")
+    private String consultRxplan;
+
+    @JsonProperty("CONSULT_PATIENTSTATUS")
+    private Long consultPatientstatus;
+
+    public Consult mapToConsult() {
+        Consult consult = new Consult();
+
+        consult.setConsultDate(Timestamp.valueOf(LocalDateTime.now()));
+        consult.setConsultSubjective(consultSubjective);
+        consult.setConsultObjective(consultObjective);
+        consult.setConsultSurvworkup(consultSurvworkup);
+        consult.setConsultRxplan(consultRxplan);
+
+        return consult;
+    }
+}
--- src/main/java/cancer/cssbackend/Entities/Requests/AddDiseaseStatusRequest.java
+++ src/main/java/cancer/cssbackend/Entities/Requests/AddDiseaseStatusRequest.java
@@ -9,14 +9,13 @@ import lombok.Setter;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
 public class AddDiseaseStatusRequest {
-    private final PatientRepository patientRepository;
-
     @JsonProperty("PATIENT_ID")
     private Long patientID;
 
@@ -25,86 +24,25 @@ public class AddDiseaseStatusRequest {
 
     @JsonProperty("DXSTATUS_SYMPTOMS")
     private char dxstatusSymptoms;
-    //@JsonProperty("PATIENT_ID")
-    //private Long patientID;
-
-    //@JsonProperty("HISTO_PATHOLOGY")
-    //private Long histoPathology;
-
-    @JsonProperty("DXSTATUS_ALIVE")
-    private char diseaseStatusAlive;
-
-    @JsonProperty("DXSTATUS_SYMPTOMS")
-    private char diseaseStatusSymptoms;
-
-    @JsonProperty("DXSTATUS_RECURRENCE")
-    private char diseaseStatusRecurrence;
-
-    @JsonProperty("DXSTATUS_METASTATIC")
-    private char diseaseStatusMetastatic;
-
-    @JsonProperty("DXSTATUS_CURATIVE")
-    private char diseaseStatusCurative;
-
-    @JsonProperty("DXSTATUS_CREATEDON")
-    private String diseaseStatusCreatedOn;
-
-    /*
-    @JsonProperty("HISTO_TUMORSIZE")
-    private int histoTumorSize;
->>>>>>> Stashed changes
 
     @JsonProperty("DXSTATUS_RECURRENCE")
     private char dxstatusRecurrence;
 
     @JsonProperty("DXSTATUS_METASTATIC")
-    private char dxstatusMetastatic;
+    private char dxstatussMetastatic;
 
     @JsonProperty("DXSTATUS_CURATIVE")
     private char dxstatusCurative;
 
-    @JsonProperty("DXSTATUS_CREATED_ON")
-    private Timestamp dxstatusCreatedOn;
-
-    public DiseaseStatus mapToDiseaseStatus(){
-        DiseaseStatus diseaseStatus = new DiseaseStatus();
-        Optional<Patient> optionalPatient = patientRepository.findById(this.patientID);
-
-        if(optionalPatient.isPresent()) {
-            Patient patient = optionalPatient.get();
-            diseaseStatus.setPatient(patient);
-            diseaseStatus.setDxstatusAlive(this.dxstatusAlive);
-            diseaseStatus.setDxstatusSymptoms(this.dxstatusSymptoms);
-            diseaseStatus.setDxstatusRecurrence(this.dxstatusRecurrence);
-            diseaseStatus.setDxstatusMetastatic(this.dxstatusMetastatic);
-            diseaseStatus.setDxstatusCurative(this.dxstatusCurative);
-
-            Timestamp currrentTimestamp = Timestamp.valueOf(LocalDateTime.now());
-            diseaseStatus.setDxstatusCreatedOn(currrentTimestamp);
-            return diseaseStatus;
-        }
-        return null;
-    @JsonProperty("HISTO_STAGE")
-    private String histoStage;
-
-    @JsonProperty("HISTO_CREATEDON")
-    private String histoCreatedOn;
-
-    @JsonProperty("HISTO_UPDATEDON")
-    private String histoUpdatedOn;
-
-    @JsonProperty("HISTO_ENCODER")
-    private Long histoEncoderID;
-    */
     public DiseaseStatus mapToDiseaseStatus(){
         DiseaseStatus diseaseStatus = new DiseaseStatus();
 
-        diseaseStatus.setDxstatusAlive(this.diseaseStatusAlive);
-        diseaseStatus.setDxstatusAlive(this.diseaseStatusSymptoms);
-        diseaseStatus.setDxstatusRecurrence(this.diseaseStatusRecurrence);
-        diseaseStatus.setDxstatusMetastatic(this.diseaseStatusMetastatic);
-        diseaseStatus.setDxstatusCurative(this.diseaseStatusCurative);
-        diseaseStatus.setDxstatusCreatedOn(Timestamp.valueOf(this.diseaseStatusCreatedOn));
+        diseaseStatus.setDxstatusAlive(dxstatusAlive);
+        diseaseStatus.setDxstatusSymptoms(dxstatusSymptoms);
+        diseaseStatus.setDxstatusRecurrence(dxstatusRecurrence);
+        diseaseStatus.setDxstatusMetastatic(dxstatussMetastatic);
+        diseaseStatus.setDxstatusCurative(dxstatusCurative);
+        diseaseStatus.setDxstatusCreatedOn(Timestamp.valueOf(LocalDateTime.now()));
 
         return diseaseStatus;
     }
