d75aa276a893802727d7bf105d8b0862d9260349
nathan callang <nbcallang@up.edu.ph>
2024-11-16 00:38:43 +0800

    [feat][nathan] - Update a message to have the read status

--- src/main/java/cancer/cssbackend/Controllers/NotificationLogController.java
+++ src/main/java/cancer/cssbackend/Controllers/NotificationLogController.java
@@ -1,10 +1,11 @@
 package cancer.cssbackend.Controllers;
 
 import cancer.cssbackend.Services.NotificationLogService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -12,4 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/notif/log")
 public class NotificationLogController {
     private final NotificationLogService notificationLogService;
+
+    @PutMapping("/{notifLogID}/status/read")
+    public ResponseEntity<String> markNotificationAsRead(@PathVariable Long notifLogID) {
+        try {
+            String message = notificationLogService.updateReadStatus(notifLogID);
+            return ResponseEntity.ok(message);
+        } catch (EntityNotFoundException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An error occurred while updating the notification status.");
+        }
+    }
+
 }
