1ecedb7c0f74a943df8378e8a0d1be6009701afa
nathan callang <nbcallang@up.edu.ph>
2024-11-29 15:19:13 +0800

    [feat][nathan] fix fetch latest disease and added api for getting othersites by disease id

--- src/main/java/cancer/cssbackend/Repositories/DiseaseRepository.java
+++ src/main/java/cancer/cssbackend/Repositories/DiseaseRepository.java
@@ -3,9 +3,16 @@ package cancer.cssbackend.Repositories;
 import cancer.cssbackend.Entities.Disease;
 import cancer.cssbackend.Entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiseaseRepository extends JpaRepository<Disease, Long> {
+
     Disease findByPatient(Patient patient);
+
+
+    @Query(value="SELECT * FROM Disease d WHERE d.PATIENT_ID = :patientID ORDER BY d.DISEASE_UPDATED_ON DESC FETCH FIRST 1 ROW ONLY;", nativeQuery = true)
+    Disease fetchByPatientID(@Param(("patientID")) Long patientID);
 }
