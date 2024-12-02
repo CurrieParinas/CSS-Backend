c86ef3ad8191c59576d0f2da6dd4ab1f2964dcd9
nathan callang <nbcallang@up.edu.ph>
2024-12-02 17:57:24 +0800

    [feat][nathan] fetch symptom name by cancer type && symptom category

--- src/main/java/cancer/cssbackend/Services/SymptomSurveyService.java
+++ src/main/java/cancer/cssbackend/Services/SymptomSurveyService.java
@@ -4,8 +4,26 @@ import cancer.cssbackend.Repositories.SymptomSurveyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class SymptomSurveyService {
     private final SymptomSurveyRepository symptomSurveyRepository;
+
+    public List<String> getSNbyCTLS(Long cancerType){
+        return symptomSurveyRepository.fetchSymptomNameByCancerTypeLOCALSYMPTOM(cancerType);
+    }
+
+    public List<String> getSNbyCTSS(Long cancerType){
+        return symptomSurveyRepository.fetchSymptomNameByCancerTypeSYSTEMICSYMPTOM(cancerType);
+    }
+
+    public List<String> getSNbyCTQOL(Long cancerType){
+        return symptomSurveyRepository.fetchSymptomNameByCancerTypeQOL(cancerType);
+    }
+
+    public List<String> getSNbyCTTSE(Long cancerType){
+        return symptomSurveyRepository.fetchSymptomNameByCancerTypeTSE(cancerType);
+    }
 }
