47cf9bd5cb21d02dd3c79cb8e54c6a22be073025
nathan callang <nbcallang@up.edu.ph>
2024-12-02 16:40:08 +0800

    [feat][nathan] - set schedule consult (post)

--- src/main/java/cancer/cssbackend/Services/CheckupScheduleService.java
+++ src/main/java/cancer/cssbackend/Services/CheckupScheduleService.java
@@ -1,6 +1,11 @@
 package cancer.cssbackend.Services;
 
+import cancer.cssbackend.Entities.CheckupSchedule;
+import cancer.cssbackend.Entities.Requests.AddCheckupScheduleRequest;
 import cancer.cssbackend.Repositories.CheckupScheduleRepository;
+import cancer.cssbackend.Repositories.CheckupStatusRepository;
+import cancer.cssbackend.Repositories.DoctorRepository;
+import cancer.cssbackend.Repositories.PatientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +13,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CheckupScheduleService {
     private final CheckupScheduleRepository checkupScheduleRepository;
+    private final PatientRepository patientRepository;
+    private final DoctorRepository doctorRepository;
+    private final CheckupStatusRepository checkupStatusRepository;
+
+    public CheckupSchedule addCheckupSchedule(AddCheckupScheduleRequest addCheckupScheduleRequest){
+        CheckupSchedule checkupSchedule = addCheckupScheduleRequest.mapToCheckupSchedule(patientRepository, doctorRepository, checkupStatusRepository);
+        checkupScheduleRepository.save(checkupSchedule);
+        return checkupSchedule;
+    }
 }
