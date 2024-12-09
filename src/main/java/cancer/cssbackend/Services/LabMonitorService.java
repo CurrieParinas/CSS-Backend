b0debfb92867ef435805b5809fc05d8a55aa7f32
Ron Brylle Gomez <136672572+rsgomez1@users.noreply.github.com>
2024-12-09 12:48:57 +0800

    [FIX][RON] Check Every 3 Hours No Issues

--- src/main/java/cancer/cssbackend/Services/LabMonitorService.java
+++ src/main/java/cancer/cssbackend/Services/LabMonitorService.java
@@ -18,8 +18,8 @@ public class LabMonitorService {
     private final LabMonitorRepository labMonitorRepository;
     private final EmailService emailService;
 
-    @Scheduled(fixedRate = 1000 * 60 * 60 * 24) // Check daily
-    public void processSchedules() throws MessagingException, IOException {
+    @Scheduled(cron = "0 0 8-16/2 * * *") // Every 2 hours from 8 AM to 4 PM
+    public void processSchedulesSendToday() throws MessagingException {
         // Fetch ongoing schedules
         List<LabMonitor> ongoingMonitoring = labMonitorRepository.findByStatus("ONGOING");
 
@@ -29,16 +29,35 @@ public class LabMonitorService {
             // Calculate months since the start date
             long monthsSinceStart = ChronoUnit.MONTHS.between(monitor.getLabSubmitted().getLabSubmissionDate().toLocalDateTime().toLocalDate(), today);
 
-            // Check if it's time to send an email
-            if (monthsSinceStart % monitor.getLabSubmitted().getWorkupName().getWorkupFrequency() == 0 && monitor.getLabSubmitted().getLabSubmissionDate().toLocalDateTime().toLocalDate() != today) {
-                emailService.sendLabEmail('r', monitor.getLabSubmitted().getPatient().getUser().getUserEmail(), "Lab Submission Reminder", monitor.getLabSubmitted().getPatient().getUser().getUserLastname(), monitor.getLabSubmitted().getDoctor().getUser().getUserLastname(), monitor.getLabSubmitted().getWorkupName().getCancerType().getBodysiteName(), monitor.getLabSubmitted().getWorkupName().getWorkupName());
+            // Check if the email should be sent today
+            if (monthsSinceStart % monitor.getLabSubmitted().getWorkupName().getWorkupFrequency() == 0 && monitor.getEmailStatus().equals("SEND")) {
+                emailService.sendLabEmail('r', monitor.getLabSubmitted().getPatient().getUser().getUserEmail(), "Lab Submission Reminder", monitor.getLabSubmitted().getPatient().getUser().getUserFirstname() + " " + monitor.getLabSubmitted().getPatient().getUser().getUserLastname(), monitor.getLabSubmitted().getDoctor().getUser().getUserFirstname() + " " + monitor.getLabSubmitted().getDoctor().getUser().getUserLastname(), monitor.getLabSubmitted().getWorkupName().getCancerType().getBodysiteName(), monitor.getLabSubmitted().getWorkupName().getWorkupName());
+                monitor.setEmailStatus("SENT");
 
                 // Check if the current date is equal to or after the end date
                 if (!today.isBefore(monitor.getEndDate().toLocalDate())) { // today >= endDate
                     monitor.setStatus("COMPLETED");
                 }
 
-                // Save updated schedule
+                labMonitorRepository.save(monitor);
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 0 17-23/2 * * *") // Every 2 hours from 5 PM to 11 PM
+    public void processSchedulesSendTomorrow() throws MessagingException {
+        // Fetch ongoing schedules
+        List<LabMonitor> ongoingMonitoring = labMonitorRepository.findByStatus("ONGOING");
+
+        for (LabMonitor monitor : ongoingMonitoring) {
+            LocalDate today = LocalDate.now();
+
+            // Calculate months since the start date
+            long monthsSinceStartTomorrow = ChronoUnit.MONTHS.between(monitor.getLabSubmitted().getLabSubmissionDate().toLocalDateTime().toLocalDate().plusDays(1), today);
+
+            //Check if the email should be sent tomorrow
+            if (monthsSinceStartTomorrow % monitor.getLabSubmitted().getWorkupName().getWorkupFrequency() == 0 && !monitor.getEmailStatus().equals("SEND")) {
+                monitor.setEmailStatus("SEND");
                 labMonitorRepository.save(monitor);
             }
         }
